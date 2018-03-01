c2819cb09a8e84ffbaec198b068a540660d38401
tran.tuan.nghia <FRAMGIA\tran.tuan.nghia@framgia0058-pc.framgia.com>
2018-03-01 18:56:59 +0700

    curd pipeline

--- app/controllers/pipelines_controller.rb
+++ app/controllers/pipelines_controller.rb
@@ -1,16 +1,38 @@
 class PipelinesController < ApplicationController
-	before_action :load_pipeline, only: :update
+	before_action :load_pipeline, only: [:update, :destroy]
 
 	def index
 		@pipelines = PipeLine.all
+		@new_pipeline = PipeLine.new
+	end
+
+	def create
+		pipeline = PipeLine.new pipeline_params
+
+		if pipeline.save
+			flash[:success] = "Create PipeLine successfully"
+		else
+			flash[:error] = "Create PipeLine error"
+		end
+		redirect_to pipelines_path
 	end
 
 	def update
 		if @pipeline.update_attributes pipeline_params
-			flash[:notice] = "Update successfully"
+			flash[:success] = "Update PipeLine successfully"
 		else
-			flash[:notice] = "Update error"
+			flash[:error] = "Update PipeLine error"
 		end
+		redirect_to pipelines_path
+	end
+
+	def destroy
+		if @pipeline.destroy
+			flash[:success] = "Delete PipeLine successfully"
+		else
+			flash[:error] = "Update PipeLine error"
+		end
+		redirect_to pipelines_path
 	end
 
 	private
